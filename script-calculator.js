b6d2adf50274c63b818e18f3b216767cf559acca
Jerzy laptop <jerzyraszka2000@gmail.com>
2024-04-16 17:07:38 +0200

    Added input after equation protection

--- script-calculator.js
+++ script-calculator.js
@@ -2,7 +2,7 @@ let number = "";
 let oldNumber = "";
 let operation;
 let counter = 0;
-let result = 0;
+let inputOff = false;
 const one = document.getElementById("one");
 const two = document.getElementById("two");
 const three = document.getElementById("three");
@@ -19,47 +19,67 @@ const multiplication = document.getElementById("multiplication");
 const division = document.getElementById("division");
 const equation = document.getElementById("equation");
 
-one.addEventListener("click", function(){
-    number+="1";
-    document.getElementById("calculatorScreen").innerHTML += "1";
-})
-two.addEventListener("click", function(){
-    number+="2";
-    document.getElementById("calculatorScreen").innerHTML += "2";
-})
-three.addEventListener("click", function(){
-    number+="3";
-    document.getElementById("calculatorScreen").innerHTML += "3";
-})
-four.addEventListener("click", function(){
-    number+="4";
-    document.getElementById("calculatorScreen").innerHTML += "4";
-})
-five.addEventListener("click", function(){
-    number+="5";
-    document.getElementById("calculatorScreen").innerHTML += "5";
-})
-six.addEventListener("click", function(){
-    number+="6";
-    document.getElementById("calculatorScreen").innerHTML += "6";
-})
-seven.addEventListener("click", function(){
-    number+="7";
-    document.getElementById("calculatorScreen").innerHTML += "7";
-})
-eight.addEventListener("click", function(){
-    number+="8";
-    document.getElementById("calculatorScreen").innerHTML += "8";
-})
-nine.addEventListener("click", function(){
-    number+="9";
-    document.getElementById("calculatorScreen").innerHTML += "9";
-})
-zero.addEventListener("click", function(){
-    number+="0";
-    document.getElementById("calculatorScreen").innerHTML += "0";
-})
 
+    one.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "1";
+            document.getElementById("calculatorScreen").innerHTML += "1";
+        }
+    })
+    two.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "2";
+            document.getElementById("calculatorScreen").innerHTML += "2";
+        }
+    })
+    three.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "3";
+            document.getElementById("calculatorScreen").innerHTML += "3";
+        }
+    })
+    four.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "4";
+            document.getElementById("calculatorScreen").innerHTML += "4";
+        }
+    })
+    five.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "5";
+            document.getElementById("calculatorScreen").innerHTML += "5";
+        }
+    })
+    six.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "6";
+            document.getElementById("calculatorScreen").innerHTML += "6";
+        }
+    })
+    seven.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "7";
+            document.getElementById("calculatorScreen").innerHTML += "7";
+        }
+    })
+    eight.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "8";
+            document.getElementById("calculatorScreen").innerHTML += "8";
+        }
+        })
+    nine.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "9";
+            document.getElementById("calculatorScreen").innerHTML += "9";
+        }
+    })
+    zero.addEventListener("click", function () {
+        if(inputOff === false) {
+            number += "0";
+            document.getElementById("calculatorScreen").innerHTML += "0";
+        }
+    })
 addition.addEventListener("click", function(){
     if(counter === 0) {
         document.getElementById("calculatorScreen").innerHTML += "+";
@@ -67,6 +87,7 @@ addition.addEventListener("click", function(){
         number = "";
         operation = 0;
         counter++;
+        inputOff = false;
     }
 })
 subtraction.addEventListener("click", function(){
@@ -76,6 +97,7 @@ subtraction.addEventListener("click", function(){
         number = "";
         operation = 1;
         counter++;
+        inputOff = false;
     }
 })
 multiplication.addEventListener("click", function(){
@@ -85,6 +107,7 @@ multiplication.addEventListener("click", function(){
         number = "";
         operation = 2;
         counter++;
+        inputOff = false;
     }
 })
 division.addEventListener("click", function(){
@@ -94,6 +117,7 @@ division.addEventListener("click", function(){
         number = "";
         operation = 3;
         counter++;
+        inputOff = false;
     }
 })
 
@@ -109,21 +133,25 @@ function showOperation(operation) {
             number = Number(oldNumber) + Number(number);
             document.getElementById("calculatorScreen").innerHTML = number;
             counter = 0;
+            inputOff = true;
             break;
         case 1:
             number = Number(oldNumber) - Number(number);
             document.getElementById("calculatorScreen").innerHTML = number;
             counter = 0;
+            inputOff = true;
             break;
         case 2:
             number = Number(oldNumber) * Number(number);
             document.getElementById("calculatorScreen").innerHTML = number;
             counter = 0;
+            inputOff = true;
             break;
         case 3:
             number = Number(oldNumber) / Number(number);
             document.getElementById("calculatorScreen").innerHTML = number;
             counter = 0;
+            inputOff = true;
             break;
     }
 }
