1b74d850682b422fbaddec4055583c67a6881955
Jerzy laptop <jerzyraszka2000@gmail.com>
2024-04-16 20:51:28 +0200

    Added clear, remove and protection for division by zero

--- script-calculator.js
+++ script-calculator.js
@@ -3,13 +3,14 @@ let oldNumber = "";
 let isSelectingSecondNumber = false;
 let isDisplayingResult = false;
 let operation;
+const screen = document.getElementById("calculator-screen");
 
 const equation = document.getElementById("equation");
 
 const onNumberSelect = (_number) =>{
     if(!isDisplayingResult) {
         number += _number;
-        document.getElementById("calculatorScreen").innerHTML += _number;
+        screen.innerHTML += _number;
     }
 }
 
@@ -17,14 +18,30 @@ const onOperationSelect = (_operation) => {
     selectOperation(_operation);
 }
 
+const onClear = ()=> {
+    location.reload();
+}
+
+const onDelete = () =>{
+    if(number !== ""){
+        screen.innerHTML = screen.innerHTML.replace(/.$/, '');
+        number = number.replace(/.$/, '');
+    }
+}
+
 const selectOperation = (_operation) =>{
     if(!isSelectingSecondNumber){
-        document.getElementById("calculatorScreen").innerHTML += _operation;
+        screen.innerHTML += _operation;
         oldNumber = number;
         isSelectingSecondNumber = true;
         number = "";
         operation = _operation;
         isDisplayingResult = false;
+    }
+    if(isSelectingSecondNumber && number === ""){
+        screen.innerHTML = screen.innerHTML.replace(/.$/, '');
+        screen.innerHTML += _operation;
+        operation = _operation;
     }
 }
 
@@ -46,10 +63,16 @@ function calculateAndShowResult() {
             number = Number(oldNumber) * Number(number);
             break;
         case '/':
-            number = Number(oldNumber) / Number(number);
+            if(Number(number) !== 0){
+                number = Number(oldNumber) / Number(number);
+            }
+            else{
+                location.reload();
+                alert("Can't divied by zero")
+            }
             break;
     }
-    document.getElementById("calculatorScreen").innerHTML = number;
+    screen.innerHTML = number;
     isSelectingSecondNumber = false;
     isDisplayingResult = true;
 }
