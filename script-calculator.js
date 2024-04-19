d78d70998ef72bc0ed6838ceebd9d8ac93f7313f
Jirru <jerzyraszk2000@gmail.com>
2024-04-19 17:28:14 +0200

    Added protection from adding multiple "." in one number and multiple leading zeros

--- script-calculator.js
+++ script-calculator.js
@@ -4,15 +4,34 @@ let isSelectingSecondNumber = false;
 let isDisplayingResult = false;
 let operation = "";
 let isOperationSelected = false;
+let isDot = false;
 const screen = document.getElementById("calculator-screen");
 
 const equation = document.getElementById("equation");
 
 const onNumberSelect = (_number) =>{
-
     if(!isDisplayingResult) {
-        number += _number;
-        screen.innerHTML += _number;
+        if(_number !== "." ) {
+            screen.innerHTML += _number;
+            if (_number === "0" && number === "") {
+                screen.innerHTML += ".";
+                number += _number + "."
+                isDot = true;
+            } else {
+                number += _number;
+            }
+        }
+        else if(!isDot) {
+            if(number === "") {
+                screen.innerHTML += "0.";
+                number += "0.";
+                isDot = true;
+            }
+            else {
+                screen.innerHTML += ".";
+                number += _number;
+            }
+        }
     }
 }
 
@@ -25,6 +44,8 @@ const onClear = ()=> {
     oldNumber = "";
     isSelectingSecondNumber = false;
     isDisplayingResult = false;
+    isDot = false;
+    isOperationSelected = false;
     operation = "";
     screen.innerHTML = "";
 }
@@ -33,10 +54,14 @@ const onDelete = () =>{
         if (!isDisplayingResult && number !== "") {
             screen.innerHTML = screen.innerHTML.replace(/.$/, '');
             number = number.replace(/.$/, '');
+            if(number.endsWith(".")){
+                isDot = false;
+            }
         }
 }
 
 const selectOperation = (_operation) =>{
+    isDot = false;
     if(_operation === "-" && number===""){
         screen.innerHTML += _operation;
         number += "-";
@@ -54,6 +79,7 @@ const selectOperation = (_operation) =>{
 
 equation.addEventListener("click", function(){
     if(isSelectingSecondNumber) {
+        isDot = false;
         calculateAndShowResult();
     }
 })
